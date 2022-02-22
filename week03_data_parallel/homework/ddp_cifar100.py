a3bc2e59fec623d4c0639dce67b84f1e585d86c4
Max Ryabinin <mryabinin0@gmail.com>
2022-02-22 13:37:49 +0300

    Update task descriptions for week03

--- week03_data_parallel/homework/ddp_cifar100.py
+++ week03_data_parallel/homework/ddp_cifar100.py
@@ -33,16 +33,13 @@ class Net(nn.Module):
         self.dropout2 = nn.Dropout(0.5)
         self.fc1 = nn.Linear(6272, 128)
         self.fc2 = nn.Linear(128, 100)
-        self.bn1 = nn.BatchNorm2d(32, affine=False)  # to be replaced with SyncBatchNorm
-        self.bn2 = nn.BatchNorm2d(32, affine=False)
+        self.bn1 = nn.BatchNorm1d(128, affine=False)  # to be replaced with SyncBatchNorm
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.bn1(x)
         x = F.relu(x)
 
         x = self.conv2(x)
-        x = self.bn2(x)
         x = F.relu(x)
 
         x = F.max_pool2d(x, 2)
@@ -50,6 +47,7 @@ class Net(nn.Module):
 
         x = torch.flatten(x, 1)
         x = self.fc1(x)
+        x = self.bn1(x)
         x = F.relu(x)
         x = self.dropout2(x)
         output = self.fc2(x)
@@ -110,4 +108,4 @@ def run_training(rank, size):
 
 if __name__ == "__main__":
     local_rank = int(os.environ["LOCAL_RANK"])
-    init_process(local_rank, fn=run_training, backend="gloo")  # replace with "nccl" afterwards
+    init_process(local_rank, fn=run_training, backend="gloo")  # replace with "nccl" when testing on GPUs
