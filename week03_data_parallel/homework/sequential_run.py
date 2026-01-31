a57f80cafce6029e4e39a80f5a8e60897a2cbf9c
Sergei Vorobyov <svorobyov@together.ai>
2026-01-31 15:15:21 +0100

    add week 3

---
+++ week03_data_parallel/homework/sequential_run.py
@@ -0,0 +1,35 @@
+import os
+
+import torch.distributed as dist
+
+
+def run_sequential(rank, size, num_iter=10):
+    """
+    Prints the process rank sequentially in two orders over `num_iter` iterations,
+    separating the output for each iteration by `---`.
+    Example (3 processes, num_iter=2):
+    ```
+    Process 0
+    Process 1
+    Process 2
+    Process 2
+    Process 1
+    Process 0
+    ---
+    Process 0
+    Process 1
+    Process 2
+    Process 2
+    Process 1
+    Process 0
+    ```
+    """
+
+    pass
+
+
+if __name__ == "__main__":
+    local_rank = int(os.environ["LOCAL_RANK"])
+    dist.init_process_group(rank=local_rank, backend="gloo")
+
+    run_sequential(local_rank, dist.get_world_size())
